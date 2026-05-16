77b75f8fba59f35107a41e239f1f4b270eff67e2
Jimmy Phan <phanjimmy02@gmail.com>
2026-05-16 15:58:03 -0400

    Function for clearing rows

--- Tetris.py
+++ Tetris.py
@@ -164,8 +164,24 @@ def draw_grid(surface, grid):
             pygame.draw.line(surface, (128,128,128), (sx, sy + i*block_size), (sx + play_width, sy + i*block_size))  # horizontal lines  
             for j in range(len(grid[i])):
                   pygame.draw.line(surface, (128,128,128), (sx + j*block_size, sy), (sx + j*block_size, sy + play_height))  # vertical lines
-def clear_rows():
-      pass
+def clear_rows(grid, locked):
+      inc = 0
+      for i in range(len(grid)-1, -1, -1):
+            row = grid[i]
+            if (0,0,0) not in row:
+                  inc += 1
+                  ind = i
+                  for j in range(len(row)):
+                        try:
+                              del locked[(j, i)]
+                        except:
+                              continue
+      if inc > 0:
+            for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+                  x, y = key
+                  if y < ind:
+                        newKey = (x, y + inc)
+                        locked[newKey] = locked.pop(key)
 def draw_next_shape(shape, surface):
       font = pygame.font.SysFont('comicsans', 30)
       label = font.render('Next Shape', 1, (255,255,255))
@@ -242,7 +258,8 @@ def main(win):
                   for pos in shape_pos:
                         p = (pos[0], pos[1])
                         locked_positions[p] = current_piece.color
-                  current_piece = get_shape()
+                  current_piece = next_piece
+                  next_piece = get_shape()
                   change_piece = False
                   clear_rows(grid, locked_positions)
             draw_window(win, grid)
